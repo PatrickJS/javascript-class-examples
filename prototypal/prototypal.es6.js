553217b953ad5633179c4d49d8096758f34cb409
PatrickJS <github@gdi2290.com>
2015-01-07 00:54:48 -0800

    Update prototypal.es6.js

--- prototypal/prototypal.es6.js
+++ prototypal/prototypal.es6.js
@@ -1,17 +1,18 @@
 import { EventEmitter } from 'events';
 
 // ES6 Prototypal
-class Person extends EventEmitter {
-  constructor() {
-    let person = Object.create(Person.prototype);
-    EventEmitter.call(person)
-    person.wallet = 0;
-    return person;  
-  }
-  talk() {
-  
-  }
+function Person() {
+  let person = Object.create(Person.prototype);
+  EventEmitter.call(person)
+  person.wallet = 0;
+  return person;  
 }
+
+Object.setPrototypeOf(Person, EventEmitter.prototype);
+ 
+Person.prototype.talk = function() {
+  
+};
 
 let person = Person();
 person.talk();
